af835d598b1b2563bf721e00ccf79bfbf3b55347
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-04-19 17:39:16 -0600

    added a component for the input of the todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import InputTodo from "./components/InputTodo";
 
 function App() {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<>
+			<InputTodo />
+		</>
 	);
 }
 
---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function InputTodo() {
+	return (
+		<div>
+			<h1>Input a Todo</h1>
+		</div>
+	);
+}
