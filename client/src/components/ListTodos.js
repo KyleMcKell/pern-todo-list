9ef2d24b9f0f0c2b5d7ffe31cd24e90c43b04f55
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-04-19 23:59:41 -0600

    added in a table to track todos

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,9 +1,57 @@
-import React from "react";
+import {
+	Paper,
+	TableCell,
+	TableContainer,
+	Table,
+	TableHead,
+	TableRow,
+	makeStyles,
+	TableBody,
+	Button,
+} from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+
+const useStyles = makeStyles({
+	table: {
+		minWidth: 650,
+	},
+	descriptionCell: {
+		width: "100%",
+	},
+});
 
 export default function ListTodos() {
+	const classes = useStyles();
+	const [todos, setTodos] = useState([
+		{ description: "Hi there" },
+		{ description: "second one" },
+	]);
+
 	return (
-		<div>
-			<h1>List Todos</h1>
-		</div>
+		<TableContainer component={Paper}>
+			<Table className={classes.table}>
+				<TableHead>
+					<TableRow>
+						<TableCell className={classes.descriptionCell}>Todos</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{todos.map((todo) => (
+						<TableRow key={uuid()}>
+							<TableCell>{todo.description}</TableCell>
+							<TableCell>
+								<Button variant="outlined">Edit</Button>
+							</TableCell>
+							<TableCell>
+								<Button variant="outlined" color="secondary">
+									Delete
+								</Button>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
 	);
 }
